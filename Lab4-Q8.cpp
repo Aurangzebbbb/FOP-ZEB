2fa416eed421e5029351b42ffe0390c812a022ab
Aurangzeb <zeb.aurang.1560@gmail.com>
2026-06-28 23:00:48 +0500

    add solved task 4.8

---
+++ Lab4-Q8.cpp
@@ -0,0 +1,33 @@
+/*
+Task 4.4 ‘Do-WHile’ Statement
+ Write a C++ program which takes an integer input from 
+user in the variable named ‘num’, calculates the sum of all integers from 1 to 
+‘num’ and displays this sum on screen. Your program should have the following interface. 
+ 
+Enter any value: 10 
+	 	Sum of integers from 1 to 10 is: 55 
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int num , sum=0 , i=1;
+	
+	cout<<"Enter an integer: ";
+	cin>>num;
+	
+	do
+	{
+		sum+=i;
+		
+		i++;
+	}while(i<=num);
+	
+	cout<<"Sum of integers from "<<1<<" to "<<num<<" is "<<sum<<endl;
+	
+	return 0;
+}
+
