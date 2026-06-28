a682886ae25e43d8a7ffdc2e50aa34726316d8cb
Aurangzeb <zeb.aurang.1560@gmail.com>
2026-06-28 22:56:09 +0500

    add solved task 4.6

---
+++ Lab4-Q6.cpp
@@ -0,0 +1,33 @@
+/*
+Task 4.2 Do-While Statement  
+Write a C++ program which prompts the user to enter a float value, repeatedly prints the 
+value of this floating point variable, decreasing it by 0.5 each time, as long as its value
+ remains greater than or equal to 0. Your program should have the following interface. 
+Enter any float value: 2.5 
+2.5 2.0 1.5 1.0 
+0.5 
+0 
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	float num;
+	
+	cout<<"Enter a number: ";
+	cin>>num;
+	
+	cout<<num<<endl;
+	
+	do
+	{
+		num-=0.5;
+		cout<<num<<endl;
+	}while(num>0);
+	
+	return 0;
+}
