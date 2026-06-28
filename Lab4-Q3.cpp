41276f1a2f2837551563bff057bd189fb5737193
Aurangzeb <zeb.aurang.1560@gmail.com>
2026-06-28 22:50:54 +0500

    add solved task 4.3

---
+++ Lab4-Q3.cpp
@@ -0,0 +1,28 @@
+/*
+		(using for loop)
+ Write a C++ program which takes an integer input from the user, calculates the factorial of this number using 
+ loop statement and displays the result on screen. Your program should have the following interface. 
+	 	Enter any value to calculate its Factorial: 5 
+Factorial of 5 is: 120 
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int num , factorial=1;
+	
+	cout<<"Enter an integer to calculate its factorial: ";
+	cin>>num;
+	
+	for(int i=1 ; i<=num ; i++)
+	{
+		factorial*=i;
+	}
+	
+	cout<<"Factorial of "<<num<<" is "<<factorial<<endl;
+	
+	return 0;
+}
