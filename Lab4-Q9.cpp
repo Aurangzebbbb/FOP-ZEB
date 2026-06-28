e3fceab4e125efd7d5d758d0ba67e8f273767c46
Aurangzeb <zeb.aurang.1560@gmail.com>
2026-06-28 23:03:17 +0500

    add solved task 4.9

---
+++ Lab4-Q9.cpp
@@ -0,0 +1,36 @@
+/*
+Task 4.5 ‘for’ Statement 
+The Greatest Common Divisor (GCD) of two integers is the largest integer that 
+evenly divides both the numbers. Write a C++ program that returns the GCD of two
+ numbers entered by the user. Your program should have the following interface. 
+	 	Enter the first number: 35 
+Enter the second number: 45 
+	 	GCD of 35 and 45 is: 5 
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int num1 , num2 , gcd;
+	
+	cout<<"Enter first number: ";
+	cin>>num1;
+	
+	cout<<"Enter second number: ";
+	cin>>num2;
+	
+	for(int i=1 ; i<=num1/2 || i<=num2/2 ; i++)
+	{
+		if(num1%i==0 && num2%i==0)
+		{
+			gcd=i;
+		}
+	}
+	
+	cout<<"GCD of "<<num1<<" and "<<num2<<" is "<<gcd<<endl;
+	
+	return 0;
+}
