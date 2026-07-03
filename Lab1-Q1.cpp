c3684734020ade5c51aceadef77e3b9818f67ede
Aurangzeb <zeb.aurang.1560@gmail.com>
2026-07-03 23:30:23 +0500

    handle invalid inputs

--- Lab1-Q1.cpp
+++ Lab1-Q1.cpp
@@ -12,6 +12,7 @@ file using internet resources and make a list of all major mathematical
 
 #include<iostream>
 #include<iomanip>
+#include<limits>
 #include<math.h>
 #include<conio.h>
 
@@ -26,6 +27,8 @@ int main()
 	cout<<"---------------------"<<endl;
 	cout<<"Scientific Calculator"<<endl;
 	cout<<"---------------------"<<endl<<endl;
+	
+	label1:
 	
 	cout<<"Press....."<<endl;
 	cout<<"1.  sin"<<endl;
@@ -44,77 +47,276 @@ int main()
 	switch(choice)
 	{
 		case '1':
-			cout<<"Enter value: ";
-			cin>>a;
+			
+			do
+			{
+				cout<<"Enter value: ";
+				cin>>a;
+				
+				if(cin.fail())
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout<<"Invalid\aInput"<<endl<<endl;
+					continue;
+				}
+				
+				else 
+				break;
+				
+			}while(true);
+			
+			
 			cout<<"The sin of "<<a<<" is "<<sin(a);
 			
 			break;
 			
 		case '2':
-			cout<<"Enter value: ";
-			cin>>a;
+			
+			do
+			{
+				cout<<"Enter value: ";
+				cin>>a;
+				
+				if(cin.fail())
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout<<"Invalid\aInput"<<endl<<endl;
+					continue;
+				}
+				
+				else 
+				break;
+				
+			}while(true);
+			
+			
 			cout<<"The cos of "<<a<<" is "<<cos(a);
 			
 			break;
 			
 		case '3':
-			cout<<"Enter value: ";
-			cin>>a;
+			
+			do
+			{
+				cout<<"Enter value: ";
+				cin>>a;
+				
+				if(cin.fail())
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout<<"Invalid\aInput"<<endl<<endl;
+					continue;
+				}
+				
+				else 
+				break;
+				
+			}while(true);
+			
+			
 			cout<<"The tan of "<<a<<" is "<<tan(a);
 			
 			break;
 			
 		case '4':
-			cout<<"Enter value: ";
-			cin>>a;
+			
+			do
+			{
+				cout<<"Enter value: ";
+				cin>>a;
+				
+				if(cin.fail())
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout<<"Invalid\aInput"<<endl<<endl;
+					continue;
+				}
+				
+				else 
+				break;
+				
+			}while(true);
+			
+			
 			cout<<"The natural logarithm of "<<a<<" is "<<log(a);
 			
 			break;
 			
 		case '5':
-			cout<<"Enter value: ";
-			cin>>a;
+			
+			do
+			{
+				cout<<"Enter value: ";
+				cin>>a;
+				
+				if(cin.fail())
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout<<"Invalid\aInput"<<endl<<endl;
+					continue;
+				}
+				
+				else 
+				break;
+				
+			}while(true);
+			
+			
 			cout<<"The common logarithm of "<<a<<" is "<<log10(a);
 			
 			break;
 			
 		case '6':
-			cout<<"Enter value: ";
-			cin>>a;
+			
+			do
+			{
+				cout<<"Enter value: ";
+				cin>>a;
+				
+				if(cin.fail())
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout<<"Invalid\aInput"<<endl<<endl;
+					continue;
+				}
+				
+				else 
+				break;
+				
+			}while(true);
+			
+			
 			cout<<"The exponential of "<<a<<" is "<<exp(a);
 			
 			break;
 			
 		case '7':
-			cout<<"Enter base: ";
-			cin>>a;
 			
-			cout<<"Enter exponent: ";
-			cin>>b;
+			do
+			{
+				cout<<"Enter base: ";
+				cin>>a;
+				
+				if(cin.fail())
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout<<"Invalid\aInput"<<endl<<endl;
+					continue;
+				}
+				
+				else 
+				break;
+				
+			}while(true);
+			
+			
+			do
+			{
+				cout<<"Enter exponent: ";
+				cin>>b;
+				
+				if(cin.fail())
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout<<"Invalid\aInput"<<endl<<endl;
+					continue;
+				}
+				
+				else 
+				break;
+				
+			}while(true);
+			
+			
 			cout<<"The answer of "<<a<<"^"<<b<<" is "<<pow(a , b);
 			
 			break;
 			
 		case '8':
-			cout<<"Enter value: ";
-			cin>>a;
+			
+			do
+			{
+				cout<<"Enter value: ";
+				cin>>a;
+				
+				if(cin.fail())
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout<<"Invalid\aInput"<<endl<<endl;
+					continue;
+				}
+				
+				else 
+				break;
+				
+			}while(true);
+			
+			
 			cout<<"The square root of "<<a<<" is "<<sqrt(a);
 			
 			break;
 			
 		case '9':
-			cout<<"Enter value: ";
-			cin>>a;
+			
+			do
+			{
+				cout<<"Enter value: ";
+				cin>>a;
+				
+				if(cin.fail())
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout<<"Invalid\aInput"<<endl<<endl;
+					continue;
+				}
+				
+				else 
+				break;
+				
+			}while(true);
+			
 			cout<<"The ceil of "<<a<<" is "<<ceil(a);
 			
 			break;
 			
 		case '0':
-			cout<<"Enter value: ";
-			cin>>a;
+			do
+			{
+				cout<<"Enter value: ";
+				cin>>a;
+				
+				if(cin.fail())
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout<<"Invalid\aInput"<<endl<<endl;
+					continue;
+				}
+				
+				else 
+				break;
+				
+			}while(true);
+			
 			cout<<"The floor of "<<a<<" is "<<floor(a);
 			
 			break;
+			
+		default:
+			
+			system("cls");
+			cout<<endl<<"Invalid \aInput"<<endl;
+			goto label1;
 		
 	}
 	
